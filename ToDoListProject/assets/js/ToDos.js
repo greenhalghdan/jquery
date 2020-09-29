76a0ba9d2f183493d7d2c4f2548953922c8c9a64
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2020-09-29 13:20:17 +0100

    Add files via upload

---
+++ ToDoListProject/assets/js/ToDos.js
@@ -0,0 +1,27 @@
+//check of specific to do by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("compleated");
+});
+
+//click onn x to delte
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//gets new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");	
+		//make new li and add to ul
+		$("ul").append("<li><span><i class = 'fa fa-trash'></i></span>" + todoText + "</li>")
+
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
